897a46358636ac68cb3a0dcc44bcff7dcf5bf666
DiegoR26 <diegoerocha26@gmail.com>
2024-07-06 11:52:07 -0300

    Map Despesas

--- src/main/java/pages/ExpensePage.java
+++ src/main/java/pages/ExpensePage.java
@@ -1,7 +1,59 @@
 package pages;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class ExpensePage extends BasePage {
+
+    @FindBy(id = "idCredor")
+    private WebElement txIdCredor;
+
+    @FindBy(id = "idUsuario")
+    private WebElement txIdUsuario;
+
+    @FindBy(id = "nomeDespesa")
+    private WebElement txNomeDespesa;
+
+    @FindBy(id = "ativo")
+    private WebElement selAtivo;
+
+    @FindBy(id = "adicionarDespesa")
+    private WebElement btnAdicionarDespesa;
+
+    @FindBy(id = "atualizarDespesa")
+    private WebElement btnAtualizarDespesa;
+
+    public ExpensePage informarIdCredor (String idCredor ) {
+        txIdCredor.sendKeys(idCredor);
+        return this;
+    }
+
+    public ExpensePage informarIdUsuario (String idUsuario) {
+        txIdUsuario.sendKeys(idUsuario);
+        return this;
+    }
+
+    public ExpensePage informarNomeDespesa (String nome) {
+        txNomeDespesa.sendKeys(nome);
+        return this;
+    }
+
+    public ExpensePage selecionarAtivo (String ativo) {
+        Select dropdown = new Select(selAtivo);
+        dropdown.selectByVisibleText(ativo);
+        return this;
+    }
+
+    public ExpensePage clicarAdicionarBase () {
+        btnAdicionarDespesa.click();
+        return this;
+    }
+
+    public ExpensePage clicarAtualizarBase () {
+        btnAtualizarDespesa.click();
+        return this;
+    }
 
     public ExpensePage(WebDriver driver) {
         super(driver);
