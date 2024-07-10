ada2fa93d2dfaf9156520363f43badd741c893a8
RenatoSchipper <renattoschipper@gmail.com>
2024-07-10 14:25:27 -0300

    Alterando Import

--- src/test/java/TesteJUnit.java
+++ src/test/java/TesteJUnit.java
@@ -3,11 +3,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 import pages.DSLPage;
 
